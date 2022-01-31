8f93dfb49d330ccedd6af81b155b633ac2386a5b
leena-Eid <lynaa388@gmail.com>
2022-02-01 01:32:22 +0200

    The main files

---
+++ dark.js
@@ -0,0 +1,10 @@
+var menu = document.querySelector('.part1formobile');
+var part1 = document.querySelector('.part1');
+/*console.log(menu);
+if (screen.width < 560) {
+    menu.style.display ='block';
+    part1.style.display='none';
+} else {
+    menu.style.display ='none';
+    part1.style.display='block';
+}*/
